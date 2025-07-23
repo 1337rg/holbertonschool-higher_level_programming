a37d07829c7a71aa5aea4c49b7c0fd096dd3880d
1337rg <remi.garzoli@protonmail.com>
2025-07-23 12:58:28 +0000

    rebase

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -2,13 +2,10 @@
 
 
 """ My module 1337rg """
-
 def print_square(size):
-    """ Method for print square """
     if type(size) is not int:
         raise TypeError('size must be an integer')
     elif size < 0:
         raise ValueError('size must be >= 0')
-
     for i in range(size):
         print(size * '#')
